1b733b42368d9a628fa1daee932b833ec97eeda5
INNELUKIPC\Denis <inneluki@gmail.com>
2018-07-24 10:48:53 +0300

    Added map and come logic to the door

---
+++ BuildingEscape/Source/BuildingEscape/OpenDoor.cpp
@@ -0,0 +1,48 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "OpenDoor.h"
+#include "Gameframework/Actor.h"
+#include "Math/Vector.h"
+#include "CoreMinimal.h"
+#include "GameFramework/Pawn.h"
+#include "Components/StaticMeshComponent.h"
+#include "GameFramework/SpringArmComponent.h"
+#include "Camera/CameraComponent.h"
+
+
+
+// Sets default values for this component's properties
+UOpenDoor::UOpenDoor()
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	PrimaryComponentTick.bCanEverTick = true;
+
+	// ...
+}
+
+
+// Called when the game starts
+void UOpenDoor::BeginPlay()
+{
+	Super::BeginPlay();
+
+	AActor* Owner = GetOwner();
+
+	FRotator NewRotation = FRotator(0.0f, 90.0f, 0.0f);
+
+	Owner->SetActorRotation(NewRotation);
+
+
+	
+}
+
+
+// Called every frame
+void UOpenDoor::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+	// ...
+}
+
--- BuildingEscape/Source/BuildingEscape/PositionReport.cpp
+++ BuildingEscape/Source/BuildingEscape/PositionReport.cpp
@@ -2,6 +2,7 @@
 
 #include "PositionReport.h"
 #include "Gameframework/Actor.h"
+#include "Math/Vector.h"
 
 
 // Sets default values for this component's properties
@@ -20,7 +21,10 @@ void UPositionReport::BeginPlay()
 {
 	Super::BeginPlay();
 	FString ObjectName = GetOwner()->GetName();
-	UE_LOG(LogTemp, Warning, TEXT("Position reporer on %s!!"), *ObjectName);
+	
+	FString ObjectPos = GetOwner()->GetTransform().GetLocation().ToString();
+
+	UE_LOG(LogTemp, Warning, TEXT("%s location is %s!!"), *ObjectName, *ObjectPos);
 	
 }
 
